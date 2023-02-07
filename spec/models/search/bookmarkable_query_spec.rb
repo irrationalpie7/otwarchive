8bf893f279e140ecab23538d583a1354811046fb
irrationalpie7 <irrationalpie7@gmail.com>
2023-02-07 12:27:27 -0800

    AO3-6471 Make bookmarkable word count filtering more efficient

--- spec/models/search/bookmarkable_query_spec.rb
+++ spec/models/search/bookmarkable_query_spec.rb
@@ -35,11 +35,19 @@ describe BookmarkableQuery do
     expect(q.generated_query[:sort]).to eq([{ word_count: { order: "asc" } }, { sort_id: { order: "asc" } }])
   end
 
+  it "allows a guest to filter by guest-visible word count" do
+    User.current_user = nil
+    q = BookmarkQuery.new(word_count: '10').bookmarkable_query
+    expect(q.generated_query.dig(:query, :bool, :filter))
+      .to include({ range: { guest_visible_word_count: { gte: 10, lte: 10 } } })
+  end
+
   it "allows a logged-in user to filter by total word count" do
     user = User.new
     user.id = 5
     User.current_user = user
     q = BookmarkQuery.new(word_count: '10').bookmarkable_query
-    expect(q.generated_query.dig(:query, :bool, :must, :has_child, :query, :bool, :filter)).to include({ range: { bookmarkable_word_count: { gte: 10, lte: 10 } } })
+    expect(q.generated_query.dig(:query, :bool, :filter))
+      .to include({ range: { word_count: { gte: 10, lte: 10 } } })
   end
 end
